067daebc5a74f2073e3f0acb37b5d98ba8e2c3b3
dudaodong <dudaodong@gmail.com>
2022-04-03 20:03:48 +0800

    feat: add three_sum

---
+++ three_sum/three_sum.go
@@ -0,0 +1,20 @@
+package threesum
+
+
+// BruteForceCount O(N) = 1/6 * N^3
+func BruteForceCount(numbers []int) int {
+	var count int
+	length := len(numbers)
+
+	for i := 0; i < length; i++ {
+		for j := i + 1; j < count; j++ {
+			for k := j + 1; k < count; k++ {
+				if numbers[i]+numbers[j]+numbers[k] == 0 {
+					count++
+				}
+			}
+		}
+	}
+
+	return count
+}
