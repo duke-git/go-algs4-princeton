269f013432f36d32c69b96cdbb57d7be475ff597
dudaodong <lanliddd.2007@163.com>
2022-04-08 11:16:14 +0800

    add ArrayQueue

--- queue/array_queue.go
+++ queue/array_queue.go
@@ -1,13 +1,49 @@
 package queue
 
 type ArrayQueue struct {
-	items  []string
-	length int
+	items    []string
+	head     int
+	tail     int
+	capacity int
+	size     int
 }
 
 func NewArrayQueue(capacity int) *ArrayQueue {
 	return &ArrayQueue{
-		items:  make([]string, 0, capacity),
-		length: 0,
+		items:    make([]string, 0, capacity),
+		head:     0,
+		tail:     0,
+		capacity: capacity,
+		size:     0,
 	}
+}
+
+func (q *ArrayQueue) IsEmpty() bool {
+	return q.size == 0
+}
+
+func (q *ArrayQueue) Enqueue(item string) bool {
+	if q.head == 0 && q.tail == q.capacity { //队列满
+		return false
+	} else if q.head != 0 && q.tail == q.capacity { //扩容
+		for i := q.head; i < q.tail; i++ {
+			q.items[i-q.head] = q.items[i]
+		}
+		q.head = 0
+		q.tail = q.tail - q.head
+	}
+
+	q.items[q.tail] = item
+	q.tail++
+	q.size++
+	return true
+}
+
+func (q *ArrayQueue) Dequeue() (string, bool) {
+	if q.head == q.tail {
+		return "", false
+	}
+	item := q.items[q.head]
+	q.head++
+	return item, true
 }
