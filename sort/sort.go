b83edd40a612908e8870df16ec437824d5a1e391
dudaodong <dudaodong@gmail.com>
2022-04-10 15:49:53 +0800

    add some sort functions

--- sort/sort.go
+++ sort/sort.go
@@ -4,6 +4,60 @@ import (
 	"go-algs4-princeton/constraint"
 )
 
+// SelectionSort O(N) = 1/2 * N^2, n times of swap
+func SelectionSort[T constraint.Number](slice []T) []T {
+	for i := 0; i < len(slice); i++ {
+		min := i
+
+		for j := i + 1; j < len(slice); j++ {
+			if slice[j] < slice[min] {
+				min = j
+			}
+		}
+		swap(slice, i, min)
+	}
+
+	return slice
+}
+
+// InsertionSort O(N) = 1/4 * N^2, 1/4 * N^2 times of swap
+// for partical sorted data, InsertionSort takes n times
+func InsertionSort[T constraint.Number](slice []T) []T {
+	for i := 0; i < len(slice); i++ {
+		for j := i; j > 0; j-- {
+			if slice[j] < slice[j-1] {
+				swap(slice, j, j-1)
+			} else {
+				break
+			}
+		}
+
+	}
+
+	return slice
+}
+
+// ShellSort O(N) = N^2/3
+func ShellSort[T constraint.Number](slice []T) []T {
+	size := len(slice)
+
+	h := 1
+	for h < size/3 {
+		h = 3*h + 1 //1, 4, 13
+	}
+
+	for h >= 1 {
+		for i := h; i < size; i++ {
+			for j := i; j >= h && slice[j] < slice[j-h]; j = j - h {
+				swap(slice, j, j-h)
+			}
+		}
+		h = h/3
+	}
+
+	return slice
+}
+
 func QuickSort[T constraint.Number](slice []T, lowIndex, highIndex int) []T {
 	if lowIndex < highIndex {
 		p := partition(slice, lowIndex, highIndex)
---
+++ sort/sort_test.go
@@ -0,0 +1,36 @@
+package sort
+
+import (
+	"go-algs4-princeton/internal"
+	"testing"
+)
+
+func TestSelectionSort(t *testing.T) {
+	assert := internal.NewAssert(t, "TestSelectionSort")
+
+	numbers := []int{5, 4, 3, 2, 1}
+
+	SelectionSort(numbers)
+
+	assert.Equal([]int{1, 2, 3, 4, 5}, numbers)
+}
+
+func TestInsertionSort(t *testing.T) {
+	assert := internal.NewAssert(t, "TestInsertionSort")
+
+	numbers := []int{5, 4, 3, 2, 1}
+
+	InsertionSort(numbers)
+
+	assert.Equal([]int{1, 2, 3, 4, 5}, numbers)
+}
+
+func TestShellSort(t *testing.T) {
+	assert := internal.NewAssert(t, "TestShellSort")
+
+	numbers := []int{5, 4, 3, 2, 1}
+
+	ShellSort(numbers)
+
+	assert.Equal([]int{1, 2, 3, 4, 5}, numbers)
+}
