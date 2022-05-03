a5e4c77c55744d4d8ba5e7c776621a45df2f5658
dudaodong <dudaodong@gmail.com>
2022-05-03 14:28:33 +0800

    add Put function

--- tree/bstree.go
+++ tree/bstree.go
@@ -7,7 +7,23 @@ type BSTree[T constraint.Comparable, U any] struct {
 }
 
 func (t *BSTree[T, U]) Put(key T, val U) {
+	t.root = put(t.root, key, val)
+}
 
+func put[T constraint.Comparable, U any](node *Node[T, U], key T, val U) *Node[T, U] {
+	if node == nil {
+		return NewNode(key, val)
+	}
+
+	if key < node.key {
+		node.left = put(node.left, key, val)
+	} else if key > node.key {
+		node.right = put(node.right, key, val)
+	} else if key == node.key {
+		node.val = val
+	}
+
+	return node
 }
 
 func (t *BSTree[T, U]) Get(key T) U {
