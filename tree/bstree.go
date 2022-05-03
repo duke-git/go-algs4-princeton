155d19e0cddcae4ce247f7dcfc9a28ffc04bee92
dudaodong <dudaodong@gmail.com>
2022-05-03 14:14:55 +0800

    add Get function

--- tree/bstree.go
+++ tree/bstree.go
@@ -3,7 +3,7 @@ package tree
 import "go-algs4-princeton/constraint"
 
 type BSTree[T constraint.Comparable, U any] struct {
-	root Node[T, U]
+	root *Node[T, U]
 }
 
 func (t *BSTree[T, U]) Put(key T, val U) {
@@ -11,7 +11,19 @@ func (t *BSTree[T, U]) Put(key T, val U) {
 }
 
 func (t *BSTree[T, U]) Get(key T) U {
+	current := t.root
 
+	for current != nil {
+		if key < current.key {
+			current = current.left
+		} else if key > current.key {
+			current = current.left
+		} else {
+			return current.val
+		}
+	}
+	var val U
+	return val
 }
 
 func (t *BSTree[T, U]) Delete(key T) {
