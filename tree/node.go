f426560b0b63028ca4fac1f709186f63f9f67ca4
dudaodong <lanliddd.2007@163.com>
2022-05-07 09:53:52 +0800

    add red-black tree

--- tree/node.go
+++ tree/node.go
@@ -10,8 +10,9 @@ type Node[T constraint.Comparable, U any] struct {
 	left  *Node[T, U]
 	right *Node[T, U]
 	count int
+	color Color // color of parent link
 }
 
 func NewNode[T constraint.Comparable, U any](key T, val U) *Node[T, U] {
-	return &Node[T, U]{key, val, nil, nil, 0}
+	return &Node[T, U]{key, val, nil, nil, 0, 0}
 }
